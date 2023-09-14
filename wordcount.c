2de83e42436589d9ee040dd1121ea35c77d55352
Zoe Rodriguez <zoe.rod9767@gmail.com>
2023-09-13 19:56:23 -0500

    now counting number of words in 1 file

--- wordcount.c
+++ wordcount.c
@@ -7,41 +7,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-// include other standard header files or your own user defined libraries needed 
-
-
+// include other standard header files or your own user defined libraries needed
 
 int main(int argc, char *arvg[])
 {
-  //take the name of an input file as a command line argument and open file argv[1] to read
-  FILE* filePointer;
+  
+  char fileCharacters;
+  FILE *filePointer;
 
   // data to be read from file
-  char readDataFromFile[50];
+  int numberOfCharacters;
 
+// take the name of an input file as a command line argument and open file argv[1] to read
   filePointer = fopen("input_file_1.txt", "r");
 
   // if file can not be opened, return/exit with exit code 1. give error
-  if (filePointer == NULL) {
+  if (filePointer == NULL)
+  {
     printf("file failed to open");
   }
-  else {
+  else
+  {
     printf("the file opened.\n");
 
     // read data from file
-    while(fgets(readDataFromFile, 50, filePointer) != NULL){
-      // print data from file
-      printf("%s", readDataFromFile);
+    while ((fileCharacters = fgetc(filePointer)) != EOF){
+      // count words
+      if(fileCharacters == ' ' || fileCharacters == '\n')
+        numberOfCharacters++;
     }
+
+
+      
+      // if file opened, count/print number of words in file argc[1] and return w/ exit code 0
+
+      // you can read file line-by-line using ReadLine() from last assignment
+      // then count words in each line, sum them up
+      // or we can use isspace(char c) to check for white-space char
+      
+      // print data from file
+      //printf("%s", readDataFromFile);
+
+    // close the file
+    fclose(filePointer);
+
+    printf("wordcount with process pid_1 counted words in File_1: number of words is %d\n", numberOfCharacters);
+    printf("file is now closed");
   }
- 
   return 0;
 }
-   
-
-
-// if file opened, count/print number of words in file argc[1] and return w/ exit code 0
-
-// you can read file line-by-line using ReadLine() from last assignment
-// then count words in each line, sum them up
-// or we can use isspace(char c) to check for white-space char
